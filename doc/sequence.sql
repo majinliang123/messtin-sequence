6767d25bbbde1f388d5599d9ed15860425cea669
xiliu <xiliu@pinduoduo.com>
2019-06-08 17:23:13 +0800

    add readme

--- doc/sequence.sql
+++ doc/sequence.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS `sequence` (
     `id` SMALLINT NOT NULL auto_increment,
     `sequence_name` varchar(60) NOT NULL,
     `current_offset` BIGINT(20) NOT NULL,
-    `modify_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(3),
     UNIQUE (`sequence_name`)
 );
 
--- src/test/java/org.messtion.sequence/Sample.java
+++ src/test/java/org.messtion.sequence/Sample.java
@@ -23,7 +23,6 @@ public class Sample {
             "    `id` SMALLINT NOT NULL auto_increment,\n" +
             "    `sequence_name` varchar(60) NOT NULL,\n" +
             "    `current_offset` BIGINT(20) UNSIGNED NOT NULL,\n" +
-            "    `modify_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(3),\n" +
             "    UNIQUE (`sequence_name`)\n" +
             ");";
 
@@ -50,7 +49,7 @@ public class Sample {
     }
 
     @Test
-    public void test() throws InterruptedException {
+    public void test() {
         Sequence sequence = new Sequence(dataSource, "abc");
 
         for (int i = 0; i < 500; i++) {
